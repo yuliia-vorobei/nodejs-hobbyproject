70f65f5ffaffcbdcdf03c5ed34015f72abca6108
Yuliia Vorobei <vorobeju1@gmail.com>
2026-01-23 17:48:10 +0100

    resolved configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
